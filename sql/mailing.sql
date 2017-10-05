16dd10c617e6c3a5b1ea0a237ea6546a0c449acc
svexican <svexican@gmail.com>
2017-10-05 11:22:13 +0200

    MAILING: created timestamp

--- sql/mailing.sql
+++ sql/mailing.sql
@@ -1,6 +1,7 @@
 create table if not exists
   mailing (
     id            uuid primary key
+  , created       date
   , email         varchar(50)
   , email_hash    bytea
   , email_sha     varchar(64) unique
@@ -17,20 +18,16 @@ grant all on mailing to rumadmin;
 create or replace function signup(email text, news boolean, release boolean, paper boolean)
 returns boolean
 language plpgsql as $$ begin
-
   insert into mailing
     (email, news, release, paper)
     values ($1, $2, $3, $4);
 
   return true;
-
 end $$;
-
 
 create or replace function encrypt_email()
 returns trigger
 language plpgsql as $$ begin
-
   if tg_op = 'INSERT' then
     new.email_sha = encode(digest(digest(new.email, 'sha256'), 'sha256'), 'hex');
     new.email_hash = pgp_pub_encrypt(new.email, dearmor(current_setting('app.pub_key')));
@@ -38,7 +35,16 @@ language plpgsql as $$ begin
 
     return new;
   end if;
+end $$;
 
+create or replace function insert_created()
+returns trigger
+language plpgsql as $$ begin
+  if tg_op = 'INSERT' then
+    new.created = current_date;
+
+    return new;
+  end if;
 end $$;
 
 create trigger encrypt_email
@@ -50,3 +56,8 @@ create trigger insert_uuid
   before insert on mailing
   for each row
   execute procedure insert_uuid();
+
+create trigger insert_created
+  before insert on mailing
+  for each row
+  execute procedure insert_created();
